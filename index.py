d0799d0af91326c0b50534addaa153cc7885a8f4
Sarah Holder <sr.holder@yahoo.com>
2020-07-21 12:59:54 -0500

    ch 18 finished

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ from animals import Goat
 from animals import Cow
 from animals import Horse
 from animals import Watersnake
-from animals import Rattlesnake
+from animals import Rattlesnake 
 from animals import Cottonmouth
 from animals import Ratsnake
 from animals import Copperhead
@@ -15,38 +15,42 @@ from animals import Catfish
 from animals import KoiFish
 
 from attractions import PettingZoo
-from attractions import SnakePit
+from attractions import SnakePit 
 from attractions import Wetlands
 
 varmint_village = PettingZoo("Varmint Village")
 the_slither_inn = SnakePit("The Slither Inn")
 critter_cove = Wetlands("Critter Cove")
 
-JA = Donkey("JA", "donkey", "morning", "straw")
-Black = Sheep("Black", "sheep", "afternoon", "hay")
-Billy = Goat("Billy", "goat", "midday", "pellets")
-Milkshake = Cow("Milkshake", "Cow", "morning", "grass")
-RedRum = Horse("RedRum", "horse", "midday", "apples")
+JA = Donkey("JA", "donkey", "morning", "straw", 334)
+Black = Sheep("Black", "sheep", "afternoon", "hay", 123)
+Billy = Goat("Billy", "goat", "midday", "pellets", 456)
+Milkshake = Cow("Milkshake", "Cow", "morning", "grass", 765)
+RedRum = Horse("RedRum", "horse", "midday", "apples", 678)
 
 varmint_village.animals.extend([JA, Black, Billy, Milkshake, RedRum])
 
-Noodles = Watersnake("Noodles", "watersnake")
-Zoe = Rattlesnake("Zoe", "rattlesnake")
-Snape = Cottonmouth("Snape", "cottonmouth")
-Buttercup = Ratsnake("Buttercup", "ratsnake")
-Sssssam = Copperhead("Sssssam", "copperhead")
+Noodles = Watersnake("Noodles", "watersnake", "fish", 198)
+Zoe = Rattlesnake("Zoe", "rattlesnake", "rabbit", 444)
+Snape = Cottonmouth("Snape", "cottonmouth", "salamander", 111)
+Buttercup = Ratsnake("Buttercup", "ratsnake", "shrew", 222)
+Sssssam = Copperhead("Sssssam", "copperhead", "mice", 555)
 
 the_slither_inn.animals.extend([Noodles, Zoe, Snape, Buttercup, Sssssam])
-
-Goldie = Goldfish("Goldie", "goldfish")
-Romeo = RedFish("Romeo", "red fish")
-Batman = BlueFish("Batman", "blue fish")
-Cleo = Catfish("Cleo", "catfish")
-Kiki = KoiFish("Kiki", "koi fish")
+ 
+Goldie = Goldfish("Goldie", "goldfish", "shelled peas", 989)
+Romeo = RedFish("Romeo", "redfish", "mullet", 454)
+Batman = BlueFish("Batman", "bluefish", "squid", 777)
+Cleo = Catfish("Cleo", "catfish", "algae", 619)
+Kiki = KoiFish("Kiki", "koi fish", "insects", 549)
 
 critter_cove.animals.extend([Goldie, Romeo, Batman, Cleo, Kiki])
 
-print(JA, JA.feed(), Black, Black.feed(), Billy, Billy.feed(), Milkshake, Milkshake.feed(), RedRum, RedRum.feed())
+Black.feed()
+Milkshake.feed()
+Romeo.feed()
+
+print(JA, Black, Billy, Milkshake, RedRum)
 print(Noodles, Zoe, Snape, Buttercup, Sssssam)
 print(Goldie, Romeo, Batman, Cleo, Kiki)
 
@@ -60,3 +64,10 @@ for animal in critter_cove.animals:
     print(f'You can find {animal.name} the {animal.species} in {critter_cove.attraction_name}')
 
 
+Noodles.chip_num = 1234321
+
+print('This is Noodle\'s chip_number: ', Noodles.chip_num)
+
+print('The last critter added was: ', the_slither_inn.last_critter_added)
+print('The last critter added was: ', critter_cove.last_critter_added)
+print('The last critter added was: ', varmint_village.last_critter_added)
